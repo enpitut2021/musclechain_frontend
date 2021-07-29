0240e7f89c2e97205ea7db7ccb8a34232e77c3d5
KeitaIsFree <maxdxd10108102@gmail.com>
2021-07-29 14:28:44 +0900

    [Add] Header bar

--- musclechain/src/App.js
+++ musclechain/src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
 import Graph from "./Graph";
+import HeaderBar from "./HeaderBar";
 
 class App extends Component {
   render() {
-    return <Graph />;
+      return (
+	  <div>
+	      <HeaderBar />
+	      <Graph />
+	  </div>
+      );
   }
 }
 
