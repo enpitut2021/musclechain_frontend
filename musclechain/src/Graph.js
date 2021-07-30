b40fb6f832d50e2952474b59256b2c0924cdbbfe
keimoriyama <keischwiiz@gmail.com>
2021-07-30 14:45:54 +0900

    [Add] show goal bar

--- musclechain/src/Graph.js
+++ musclechain/src/Graph.js
@@ -24,8 +24,6 @@ class Graph extends Component {
       graph_data: [],
       user: "",
     };
-
-    console.log(this.props.goal);
   }
 
   change_date_format(date_string) {
@@ -52,21 +50,38 @@ class Graph extends Component {
       };
       graph_datas.push(label);
     }
+    console.log("process data");
     console.log(graph_datas);
-    this.setState({
-      graph_data: graph_datas,
-    });
+    // this.setState({
+    //   graph_data: graph_datas,
+    // });
+    return graph_datas;
   }
 
-  handle_goal(goal) {
+  handle_activity(activity) {
+    console.log(activity.activity);
+    return this.process_json(activity.activity);
+  }
+
+  handle_goal(goal, data) {
     console.log(goal);
+    var goal_data = [];
+    for (var item in data) {
+      // console.log(data[item]["x"]);
+      var x_data = data[item]["x"];
+      var y_data = goal;
+      var label = { x: x_data, y: y_data };
+      goal_data.push(label);
+    }
+    return goal_data;
   }
 
   render() {
     // データの処理を追加する
-    this.handle_goal(this.props.goal);
+    const data = this.handle_activity(this.props.activity);
+    const goal = this.handle_goal(this.props.goal, data);
     return (
-      <XYPlot height={300} width={500} xType="ordinal">
+      <XYPlot height={300} width={1000} xType="ordinal">
         <VerticalGridLines />
         <HorizontalGridLines />
         <XAxis title="date" position="end" />
@@ -77,7 +92,8 @@ class Graph extends Component {
             transform: "rotate(90)",
           }}
         />
-        <LineSeries data={this.graph_data} />
+        <LineSeries data={data} />
+        <LineSeries data={goal} />
       </XYPlot>
       // <div>{this.props.goal}</div>
     );
