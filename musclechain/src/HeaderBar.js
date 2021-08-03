b44ac88d1b550dfbf199773df44503c98ec136db
KeitaIsFree <maxdxd10108102@gmail.com>
2021-08-03 13:16:58 +0900

    [Fix] ヘッダー直した

--- musclechain/src/HeaderBar.js
+++ musclechain/src/HeaderBar.js
@@ -11,23 +11,38 @@ class HeaderBar extends Component {
 			fontWeight: 'normal',
 			margin: 0,
 			padding: 10,
+			paddingRight: 20,
+			paddingBottom: 20,
 			position: 'absolute',
 			left: 0,
 			top: 0,
 			background: '#FF3333',
 			color: 'white',
-			borderBottomRightRadius: 16}}><b>Muscle</b>Chain</h1>
+			borderBottomRightRadius: 32}}><b>Muscle</b><small>CHAIN</small></h1>
 		<div style={{
-			 background: 'grey',
+			 background: '#330000',
 			 margin: 0, padding: 5,
 			 display: 'flex',
 			 justifyContent: 'space-between',
-			 alignItems: 'center'}}>
+			 alignItems: 'center',
+			 height: 50}}>
+		    <h1 style={{
+			    visibility: 'hidden',
+			fontSize: 64,
+			fontWeight: 'normal',
+			margin: 0,
+			padding: 10,
+			paddingRight: 20,
+			paddingBottom: 20,
+			background: '#FF3333',
+			color: 'white',
+			borderBottomRightRadius: 32}}><b>Muscle</b>Chain</h1>
 		    
 		    <div style={{
 			     display: 'flex',
 			     margin: 0,
-			     padding: 5}}>
+			     padding: 5,
+			     paddingRight: 40}}>
 			<h1 style={{ color: 'white', fontSize: 20, paddingLeft: 30, paddingRight: 30 }}>Balance</h1>
 			<h1 style={{ color: 'white', fontSize: 20, paddingLeft: 30, paddingRight: 30 }}>Activity</h1>
 			<h1 style={{ color: 'white', fontSize: 20, paddingLeft: 30, paddingRight: 0 }}>Goals</h1>
