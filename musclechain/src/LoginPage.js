3d97644221924fef77aea216bb368109b562125c
KeitaIsFree <maxdxd10108102@gmail.com>
2021-08-04 10:36:09 +0900

    [fix] link fix

--- musclechain/src/LoginPage.js
+++ musclechain/src/LoginPage.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import { Link, BrowserRouter, Redirect } from "react-router-dom";
 
-const api_url = "http://beae3e33ce88.ngrok.io/";
+const api_url = "http://0361b610922f.ngrok.io/";
 
 class LoginPage extends Component {
     constructor() {
